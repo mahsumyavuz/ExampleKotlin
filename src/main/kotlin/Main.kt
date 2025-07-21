39bac1e92296c813d51edfb71d6e294d6524cd2d
Mahsum YAVUZ <mahsumyavuz@gmail.com>
2025-07-21 15:56:07 +0300

    First commit with <3

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val name = "Muhammed Çağan"
+    val surname: String
+    val age = 34
+    surname="YAVUZ"
+    println(age)
+    println(name+surname)
+    var isMale=true
+
+    println(isMale)
+}
+
